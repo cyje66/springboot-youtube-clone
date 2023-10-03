daf9cc2c619e1d4efae200ae7309b086fb43970d
Eddie Chien <eddie71677@gmail.com>
2023-10-03 18:22:15 +0800

    add two APIs: uploadThumbnail(), editVideoMetadata() edit uploadVideo()

---
+++ backend/src/main/java/com/eddie/youtubeclone/dto/VideoDto.java
@@ -0,0 +1,21 @@
+package com.eddie.youtubeclone.dto;
+
+import com.eddie.youtubeclone.model.VideoStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoDto {
+    private String id;
+    private String title;
+    private String description;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private String thumbnailUrl;
+}
