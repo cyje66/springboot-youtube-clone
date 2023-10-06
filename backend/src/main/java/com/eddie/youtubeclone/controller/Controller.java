231d55f4783359ad46705c301cfa95dc17cd772d
Eddie Chien <eddie71677@gmail.com>
2023-10-06 16:20:37 +0800

    part 13: add getVideoDetails API (with bearer token), OAuth0 settings and cors settings

--- backend/src/main/java/com/eddie/youtubeclone/controller/Controller.java
+++ backend/src/main/java/com/eddie/youtubeclone/controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/test")
+    @GetMapping("/")
     public String getTest() {
         return "Hello";
     }
